27fe12f4c23146f86d92b69d08404abd89f444d2
Ronycley Agra <137537937+ronycleyagrat@users.noreply.github.com>
2023-06-30 15:27:16 -0300

    Delete TESTSUN/forms/registro_entradasaida directory

--- TESTSUN/forms/registro_entradasaida/events/displayFields.js
+++
@@ -1,3 +0,0 @@
-function displayFields(form, customHTML) {
-	customHTML.append("<script> var FORMMODE = '" + form.getFormMode() + "';</script>");
-}
--- TESTSUN/forms/registro_entradasaida/js/util.js
+++
@@ -1,8 +0,0 @@
-function util(){}
-
-//REMOVE ESPACAMENTO ENTRE LINHAS DO PAI E FILHO
-util.layout = function() {
-	$(".fs-md-space").each(function () {
-		$(this).removeClass("fs-md-space");
-	});
-};
